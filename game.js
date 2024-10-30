c17b978cfd6d07d2c56beaee384412b4b1194054
Moe-Karim <mohamadkarimfahs@gmail.com>
2024-10-30 14:03:03 +0200

    Created: create function,linked images & walls

--- game.js
+++ game.js
@@ -52,3 +52,82 @@ function preload(){
     this.load.image('hazmat', 'assets/characters/animations0054.png',)
     this.load.image('explorer', 'assets/characters/explorer0065.png',)
 }
+
+function create(){
+    //ground
+    this.add.image((400),(97.5),'mid-up')
+    this.add.image((400),(300),'mid')
+    this.add.image((400),(502.5),'mid-down')
+
+    //plants
+    this.add.image((400),(85.5),'plant-edge-up')
+    this.add.image((400),(517.5),'plant-edge-down')
+    for(var i =0 ; i<12;i++){
+        this.add.image(
+        Math.floor(Math.random() * 800 + 1),
+        (Math.floor(Math.random() * 65 + 517)),
+        'plant-water');
+
+    this.add.image(
+        Math.floor(Math.random() * 800 + 1),
+        (Math.floor(Math.random() * 80 + 2)),
+        'plant-water');
+
+    this.add.image(
+        Math.floor(Math.random() * 800 + 1),
+        (Math.floor(Math.random() * 450 + 90)),
+        'plant-temp');
+    this.add.image(
+        Math.floor(Math.random() * 800 + 1),
+        (Math.floor(Math.random() * 450 + 90)),
+        'plant-temp2');
+    this.add.image(
+        Math.floor(Math.random() * 800 + 1),
+        (Math.floor(Math.random() * 450 + 90)),
+        'plant-temp3');
+}
+platform = this.physics.add.staticGroup();
+
+    //maze
+platform.create((15),(97.5),'left-up')
+platform.create((15),(502.5),'left-down')
+platform.create((785),(97.5),'right-up')
+platform.create((785),(502.5),'right-down')
+platform.create((45),(340.5),'vertical-wall')
+platform.create((145),(190.5),'horiz-wall').setScale(0.8).refreshBody();
+platform.create((175),(120.5),'horiz-wall')
+platform.create((400),(120.5),'horiz-wall').setScale(0.5).refreshBody();
+platform.create((625),(120.5),'horiz-wall')
+platform.create((255),(270.5),'vertical-wall').setScale(0.5).refreshBody();
+platform.create((190),(350),'horiz-wall').setScale(0.5).refreshBody();
+platform.create((125),(300),'vertical-wall').setScale(0.3).refreshBody();
+platform.create((660),(205),'vertical-wall').setScale(0.5).refreshBody();
+platform.create((420),(190),'horiz-wall').setScale(0.5).refreshBody();
+platform.create((420),(200),'vertical-wall').setScale(0.5).refreshBody();
+platform.create((420),(280),'horiz-wall').setScale(0.5).refreshBody();
+platform.create((610),(190),'horiz-wall').setScale(0.3).refreshBody();
+platform.create((570),(220),'vertical-wall').setScale(0.6).refreshBody();
+//bottom
+platform.create((755),(270),'vertical-wall')
+platform.create((145),(190.5),'horiz-wall').setScale(0.8).refreshBody();
+platform.create((175),(470.5),'horiz-wall')
+platform.create((400),(470.5),'horiz-wall').setScale(0.5).refreshBody();
+platform.create((625),(470.5),'horiz-wall')
+platform.create((315),(410),'vertical-wall').setScale(0.3).refreshBody();
+platform.create((240),(410),'horiz-wall').setScale(0.5).refreshBody();
+platform.create((465),(380),'horiz-wall')
+platform.create((400),(430),'vertical-wall').setScale(0.25).refreshBody();
+platform.create((680),(410),'vertical-wall').setScale(0.3).refreshBody();
+
+//sprite
+player = this.physics.add.sprite(25, 150,sprites[spriteNum]);
+player.setCollideWorldBounds(true);
+this.physics.add.collider(player, platform);
+player.setSize(20,35);
+
+//game
+cursors = this.input.keyboard.createCursorKeys();
+spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+escapeKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+
+}
